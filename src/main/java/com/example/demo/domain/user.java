f033c0c3a3f2dae7fee5fff3f37b98e8458a8518
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-19 23:15:27 +0100

    create user entity

---
+++ src/main/java/com/example/demo/domain/user.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class user implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String email;
+    private String password;
+/*
+    private Role role;
+*/
+
+}
