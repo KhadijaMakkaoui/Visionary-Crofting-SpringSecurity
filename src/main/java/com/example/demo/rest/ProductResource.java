3b5261458cea5ec924b43bf3bb63f2d2da587d03
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-20 16:51:09 +0100

    Create RoleType enum

--- src/main/java/com/example/demo/rest/ProductResource.java
+++ src/main/java/com/example/demo/rest/ProductResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/products")
 public class ProductResource {
 
     ProductService produitService;
